4e5dc1b0bbe40516b484995c628566e80bdd410f
JangIkhwan <bluensky0213@gmail.com>
2025-11-17 13:47:01 +0900

    feat: 열람실 좌석 현황 조회 로직 작성 (green)
    - 열람실 좌석 현황 조회 로직 작성

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -6,8 +6,29 @@ import library.view.UserInputView
 
 
 fun main() {
+    val seats : Array<Boolean> = Array<Boolean>(50) { false }
+
     val userInputView = UserInputView(UserInputParser())
     userInputView.printMenu()
     val option: MenuOption = userInputView.getOption()
+    if(option.value == 1){
+        printReadingRoom(seats)
+    }
+}
+
+fun printReadingRoom(seats : Array<Boolean>){
+    println("열람실 좌석 현황")
+    for(i in 0..seats.size - 1){
+        print("${i + 1}:")
+        if(seats.get(i) == true){
+            print("[X]  ")
+        }
+        if(seats.get(i) == false){
+            print("[O]  ")
+        }
+        if((i + 1) % 10 == 0){
+            println()
+        }
+    }
 }
 
