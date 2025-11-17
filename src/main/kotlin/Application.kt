17345fd931568f68d93109eddc3679aa251af331
JangIkhwan <bluensky0213@gmail.com>
2025-11-17 11:50:06 +0900

    feat: 사용자 프롬프트 입력 로직 구현 (green)
    - 테스트를 통과하도록 사용자 프롬프트 입력 로직 구현

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,40 @@
 package library
 
-fun main() {
+import camp.nextstep.edu.missionutils.Console
 
+
+fun main() {
+    printMenu()
+    val option: Int = getOption()
+    println(option)
+}
+
+fun printMenu(){
+    println("사용자 프롬프트")
+    println("1. 도서관 좌석 조회")
+    println("2. 좌석 배정")
+    println("3. 좌석 반납")
+}
+
+fun getOption(): Int{
+    var option: Int
+    while(true){
+        print("선택하세요 > ")
+        val input: String = Console.readLine()
+        if(input.isEmpty()){
+            println("[ERROR] 입력이 비어있을 수 없습니다")
+            continue
+        }
+        try{
+            option = input.toInt()
+            if(option >= 1 && option <= 3){
+                break
+            }
+            println("[ERROR] 존재하지 않는 메뉴 번호입니다")
+        }
+        catch (e: NumberFormatException){
+            println("[ERROR] 메뉴 번호는 정수여야 합니다")
+        }
+    }
+    return option
 }
