bfc79473968641015c78cc746363019b8b4a5877
JangIkhwan <bluensky0213@gmail.com>
2025-11-17 11:41:47 +0900

    test: 사용자 메뉴 입력 테스트 작성 (red)
    - 사용자 메뉴 입력 관련 테스트 케이스 작성

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,2 +1,49 @@
-class ApplicationTest {
+package library
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ApplicationTest : NsTest() {
+    @Test
+    fun `사용자 메뉴 번호는 숫자여야 한다`(){
+        assertSimpleTest {
+            run("1")
+            assertThat(output()).doesNotContain(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `사용자 메뉴 번호가 숫자가 아니면 에러메시지가 출력된다`(){
+        assertSimpleTest {
+            runException("one")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `사용자 메뉴가 비어있으면 에러메시지가 출력된다`(){
+        assertSimpleTest {
+            runException("\n")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    @Test
+    fun `사용자 메뉴 번호의 범위를 벗어나면 에러 메시지가 출력된다`(){
+        assertSimpleTest {
+            runException("-1")
+            assertThat(output()).contains(ERROR_MESSAGE)
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE: String = "[ERROR]"
+    }
+
 }
