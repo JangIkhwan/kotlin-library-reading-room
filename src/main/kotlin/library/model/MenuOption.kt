a1f3b67c0bde0356bea48ebec0cf9293acd65bf0
JangIkhwan <bluensky0213@gmail.com>
2025-11-19 01:39:48 +0900

    fix: MenuOption과 ReadingRoom 수정

--- src/main/kotlin/library/model/MenuOption.kt
+++ src/main/kotlin/library/model/MenuOption.kt
@@ -1,6 +1,6 @@
 package library.model
 
-class MenuOption(val value: Int){
+data class MenuOption(val value: Int){
     init{
         require(value >= 1 && value <= 3, { "[ERROR] 존재하지 않는 메뉴 번호입니다" })
     }
--- src/main/kotlin/library/model/ReadingRoom.kt
+++ src/main/kotlin/library/model/ReadingRoom.kt
@@ -4,6 +4,7 @@ class ReadingRoom(size: Int) {
     val seats : Array<Boolean>
 
     init{
+        require(size > 0, { "좌석은 1개 이상이어야 합니다." })
         seats = Array<Boolean>(size) { false }
     }
 
