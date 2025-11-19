815b25f4235ab3d2b0729ecdad15db14e21bf931
JangIkhwan <bluensky0213@gmail.com>
2025-11-19 10:50:25 +0900

    test: 누락된 테스트 케이스 추가
    - 존재하지 않는 좌석에 대한 예외 테스트 추가

--- src/test/kotlin/library/model/ReadingRoomTest.kt
+++ src/test/kotlin/library/model/ReadingRoomTest.kt
@@ -16,4 +16,14 @@ class ReadingRoomTest {
         // when & then
         assertThrows<IllegalArgumentException> { readingRoom.reserve(seatNumber) }
     }
+
+    @Test
+    fun `존재하지 않는 좌석을 배정하려고 하면 예외가 발생한다`(){
+        // given
+        val readingRoom = ReadingRoom(50)
+        val seatNumber = SeatNumber(51)
+
+        // when & then
+        assertThrows<IllegalArgumentException> { readingRoom.reserve(seatNumber) }
+    }
 }
