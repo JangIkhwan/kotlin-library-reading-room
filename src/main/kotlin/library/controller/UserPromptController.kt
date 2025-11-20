2fadfb196dc3f79ef6a1388acf065a164aaf3c2f
JangIkhwan <bluensky0213@gmail.com>
2025-11-20 20:38:07 +0900

    refactor: 들여쓰기 수정

--- src/main/kotlin/library/controller/UserPromptController.kt
+++ src/main/kotlin/library/controller/UserPromptController.kt
@@ -5,8 +5,7 @@ import library.model.ReadingRoom
 import library.view.UserInputView
 import library.view.UserOutputView
 
-class UserPromptController(val inputView: UserInputView, val readingRoom: ReadingRoom, val outputView: UserOutputView) :
-    Controller {
+class UserPromptController(val inputView: UserInputView, val readingRoom: ReadingRoom, val outputView: UserOutputView) : Controller {
     var commandMap: MutableMap<MenuOption, String>
 
     init{
